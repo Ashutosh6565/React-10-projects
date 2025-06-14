7754bdc3539cfa33dc1abd2d59bb7a771bf435f5
ashutosh6565 <kumarashutosh7302401472.com>
2025-06-14 15:07:52 +0530

    Added new project just done to clear and revise my basics

---
+++ project-1/src/components/Herosection.jsx
@@ -0,0 +1,28 @@
+const Heroscetion = () => {
+  return (
+    
+      <main className="hero">
+        <div className="content">
+          <h1>YOUR FEET DESERVE THE BEST</h1>
+          <p>YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.</p>
+          <div className="but">
+            <button>
+                Shop Now
+            </button>
+            <button>
+                Category
+            </button>
+          </div>
+          <p>Also Avilable on </p>
+          <img src="" alt="" />
+          <img src="" alt="" />
+        </div>
+        <div className="imgage">
+            <img src="/src/assets/shoe_image.png" alt="" />
+        </div>
+      </main>
+    
+  );
+};
+
+export default Heroscetion;
---
+++ project-1/src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+
+const Navbar = () => {
+    return <>
+        <div className='container'>
+        <nav>
+          <img src="/src/assets/brand_logo.png" alt=""/>
+          <ul>
+            <li>Menu</li>
+            <li>About</li>
+            <li>Location</li>
+            <li>Contact</li>
+          </ul>
+          <button>Login</button>
+        </nav>
+      </div>
+    </>
+}
+
+export default Navbar;
